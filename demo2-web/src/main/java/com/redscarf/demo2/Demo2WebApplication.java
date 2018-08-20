1656b314b6074bed38c1278452416d4b144f37a7
LeeJohn <lidx@communet.io>
2018-08-20 10:50:31 +0800

    引入【spring-boot-starter-security】，增加 [http basic authentication]认证方式

--- demo2-web/src/main/java/com/redscarf/demo2/Demo2WebApplication.java
+++ demo2-web/src/main/java/com/redscarf/demo2/Demo2WebApplication.java
@@ -1,7 +1,9 @@
 package com.redscarf.demo2;
 
 import com.redscarf.demo2.persistence.mongo.repository.impl.SimpleCustomerRepository;
+import com.redscarf.demo2.service.SampleService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -19,10 +21,14 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @SpringBootApplication
 @Slf4j
 @EnableMongoRepositories(repositoryBaseClass = SimpleCustomerRepository.class)//mongo 模块才需要引进
+//@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class Demo2WebApplication implements CommandLineRunner {
 
     @Value("${applicationName}")
     private String applicationName;
+
+    @Autowired
+    private SampleService service;
 
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(Demo2WebApplication.class);
@@ -31,6 +37,18 @@ public class Demo2WebApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+        /* 权限设置
+//        SecurityContextHolder.getContext()
+//                .setAuthentication(new UsernamePasswordAuthenticationToken("user", "N/A",
+//                        AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER")));
+        try {
+            System.out.println(this.service.secure());
+        }
+        finally {
+            SecurityContextHolder.clearContext();
+        }
+*/
 //        CountDownLatch countDownLatch = new CountDownLatch(1);
         log.info("{} boot successfully", this.applicationName);
 //        countDownLatch.await();
